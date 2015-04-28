b68e8936c80490aee043da9da09e6c8af39f7050
pkmoney1138 <pkmoney@comcast.net>
2015-04-28 17:55:44 -0400

    Added a questions array with all of the question objects inside.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
+	//quiz question array
+	var game-questions = [{
+		question: "Jennifer Walters a.k.a She-Hulk is related to what famous Marvel character?",
+		choices: ["Peter Parker", "Reed Richards", "Bruce Banner", "Richard Rider"],
+		questNumb: 0,
+		correct: 2,
+		},
+		{
+		question: "Ms/Captain Marvel, Carol Danvers, began her career by joining what organization?",
+		choices: ["Airforce", "Army", "F.B.I.", "Navy"],
+		questNumb: 1,
+		correct: 0,
+		},
+		{
+		question: "Emma Frost dated which original member of the X-Men....you're not even reading this, are you? Can't blame ya. The answers is Scott Summers",
+		choices: ["Bobby Drake", "Charles Xavier", "Scott Summers", "Jean Grey...wait, what?!?!" ],
+		questNumb: 2,
+		correct: 2,
+		},
+		{
+		question: "Which one of these women is the original (and still the most popular) Spider-Woman?",
+		choices: ["Jennifer Walters", "Jean Grey", "Carol Danvers", "Jessica Drew"],
+		questNumb: 3,
+		correct: 3,
+	}]
+	//start-button code
 	$('.start-button').on('click', function() {
 		console.log('click');
 		$('.cover-page').hide();
