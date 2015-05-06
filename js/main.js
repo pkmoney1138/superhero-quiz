8c99dcdb78c82e561d6973ce4563c91ef739f2c4
pkmoney1138 <pkmoney@comcast.net>
2015-05-06 17:51:39 -0400

    something went wrong with last version, this is clean reset of simple version of quiz that works

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
 	$('.she-hulk-question .answer-buttons').on('click', function() {
 		console.log('click1');
 		questionAnswers();		
-		$('#she-hulk-correct').fadeIn();
+		$('.she-hulk-answers').fadeIn();
 		$('.she-hulk-question').fadeOut();
 	});
 	$('.she-hulk-bubbles .continue-buttons').on('click', function() {
