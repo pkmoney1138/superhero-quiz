236d822f60cf4a7103a61b0ce886d8aa985c3922
pkmoney1138 <pkmoney@comcast.net>
2015-05-07 17:32:52 -0400

    Quiz works, I think. Numb correct shows up on last page. hover effect in place for buttons. Only one color effect, though. probably will have to refactor code, but other than that. I think it's pretty close.

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
 		},
 		{
 		"question": "Ms/Captain Marvel, Carol Danvers, began her career by joining what organization?",
-		"choices": ["Airforce", "Army", "F.B.I.", "Navy"],
+		"choices": ["Airforce", "NASA", "F.B.I.", "Navy"],
 		"questNumb": 1,
 		"correct": 0,
 		},
 		{
-		"question": "Emma Frost dated which original member of the X-Men....you're not even reading this, are you? The answer is Scott Summers",
+		"question": "Emma Frost is in a relationship with which original member of the X-Men....you're not even reading this, are you? The answer is Scott Summers",
 		"choices": ["Bobby Drake", "Charles Xavier", "Scott Summers", "Jean Grey...wait, what?!?!" ],
 		"questNumb": 2,
 		"correct": 2,
@@ -50,9 +50,29 @@ $(document).ready(function() {
 	});
 	$('.she-hulk-question .answer-buttons').on('click', function() {
 		console.log('click1');
-		questionAnswers();		
+		var answers = $("input[type='radio']:checked").val();
 		$('.she-hulk-answers').fadeIn();
-		$('.she-hulk-question').fadeOut();
+		if(answers == gameQuestions[currentQuestion].correct) {
+			$('.she-hulk-question').fadeOut();
+			$('.she-hulk-bubbles p').text("Yes! Bruce is my cousin and also the reason I have my powers!")
+			numbCorrect++
+		}
+		else if(answers == 0) {
+			$('.she-hulk-question').fadeOut();
+			$('.she-hulk-bubbles p').text("Peter Parker? Spider-Man? Good friend, yes. Am I related to him? No.");
+		}
+		else if(answers == 1) {
+			$('.she-hulk-question').fadeOut();
+			$('.she-hulk-bubbles p').text("Mr. Fantastic? I've been a part of the Fantastic Four, but Reed and I are not related.")
+		}
+		else if(answers == 3) {
+			$('.she-hulk-question').fadeOut();
+			$('.she-hulk-bubbles p').text("Nova's awesome, but no we're not relations.")
+		}
+		else {
+			$('.she-hulk-bubbles p').text("How about actually choosing an answer?")
+		};
+		$("input[type='radio']").removeAttr("checked");
 	});
 	$('.she-hulk-bubbles .continue-buttons').on('click', function() {
 		console.log('click11');
@@ -62,9 +82,29 @@ $(document).ready(function() {
 	});
 	$('.captain-marvel-question .answer-buttons').on('click', function() {
 		console.log('click2');
-		questionAnswers();
+		var answers = $("input[type='radio']:checked").val();
 		$('.captain-marvel-answers').fadeIn();
-		$('.captain-marvel-question').fadeOut();
+		if(answers == gameQuestions[currentQuestion].correct) {
+			$('.captain-marvel-question').fadeOut();
+			$('.captain-marvel-bubbles p').text("Absolutely correct. It's also during my time at the airforce that Captain Marvel saved my life and I inherited his genetic structure and my powers.")	
+			numbCorrect++
+		}
+		else if(answers == 1) {
+			$('.captain-marvel-question').fadeOut();
+			$('.captain-marvel-bubbles p').text("Nope. I did work for NASA at one point, but I didn't start my career there.")
+		}
+		else if(answers == 2) {
+			$('.captain-marvel-question').fadeOut();
+			$('.captain-marvel-bubbles p').text("No, can't say I was ever in the F.B.I.")
+		}
+		else if(answers == 3) {
+			$('.captain-marvel-question').fadeOut();
+			$('.captain-marvel-bubbles p').text("I'm a fantastic pilot, not a fantastic swimmer.")
+		}
+		else {
+			$('.captain-marvel-bubbles p').text("You know what would be cool? If you actually selected an answer before hitting the answer button!")
+		}
+		$("input[type='radio']").removeAttr("checked");
 	});
 	$('.captain-marvel-bubbles .continue-buttons').on('click', function() {
 		console.log('click22');
@@ -74,9 +114,29 @@ $(document).ready(function() {
 	});
 	$('.emma-frost-question .answer-buttons').on('click', function() {
 		console.log('click3');
-		questionAnswers();
+		var answers = $("input[type='radio']:checked").val();
 		$('.emma-frost-answers').fadeIn();
-		$('.emma-frost-question').fadeOut();
+		if(answers == gameQuestions[currentQuestion].correct) {
+			$('.emma-frost-question').fadeOut();
+			$('.emma-frost-bubbles p').text("Why do you know so much about my love life? Yes, Cyclops and I officially became an item when I was the headmistress at the Xavier Institute. What were we before that? Shut up. That's what.")
+			numbCorrect++
+		}
+		else if(answers == 0) {
+			$('.emma-frost-question').fadeOut();
+			$('.emma-frost-bubbles p').text("Look, I may have strong psychic powers that can bend a man's will, but even I wouldn't be able to pull that off!");
+		}
+		else if(answers == 1) {
+			$('.emma-frost-question').fadeOut();
+			$('.emma-frost-bubbles p').text("Um. No.")
+		}
+		else if(answers == 3) {
+			$('.emma-frost-question').fadeOut();
+			$('.emma-frost-bubbles p').text("Pervert!");
+		}
+		else {
+			$('.emma-frost-bubbles p').text("My psionic abilities are telling me that you didn't make a selection.")
+		}
+		$("input[type='radio']").removeAttr("checked");
 	});
 	$('.emma-frost-bubbles .continue-buttons').on('click', function() {
 		console.log('click33');
@@ -86,13 +146,34 @@ $(document).ready(function() {
 	});
 	$('.spider-woman-question .answer-buttons').on('click', function() {
 		console.log('click4');
-		questionAnswers();
+		var answers = $("input[type='radio']:checked").val();
 		$('.spider-woman-answers').fadeIn();
-		$('.spider-woman-question').fadeOut();
+		if(answers == gameQuestions[currentQuestion].correct) {
+			$('.spider-woman-question').fadeOut();
+			$('.spider-woman-bubbles p').text("Named after Wolverine's daughter and Nancy Drew, I've survived and overcome series cancellations and usurpers to my name only to remain the one, true Spider-Woman!")
+			numbCorrect++;
+		}
+		else if(answers == 0) {
+			$('.spider-woman-question').fadeOut();
+			$('.spider-woman-bubbles p').text("Jennifer Walters is She-Hulk. Haven't you been paying attention?");
+		}
+		else if(answers == 1) {
+			$('.spider-woman-question').fadeOut();
+			$('.spider-woman-bubbles p').text("No. Jean Grey is one of the original X-Men.");
+		}
+		else if(answers == 2) {
+			$('.spider-woman-question').fadeOut();
+			$('.spider-woman-bubbles p').text("Nope. Why don't you go back to the second question to remind yourself who Carol Danvers actually is?")
+		}
+		else {
+			$('.spider-woman-bubbles p').text("By now you really should know that you need to make a selection first!")
+		}
+		$("input[type='radio']").removeAttr("checked");
 	});
 	$('.spider-woman-bubbles .continue-buttons').on('click', function() {
 		console.log('click44');
 		$('.spider-woman-page').hide();
+		$('.results p').html("Congratulations! <br> You finished the quiz and got " + numbCorrect + " out of 4 questions correct!")
 		$('.last-page').show();
 	});
 })
